d8e0f081d9f7eaeb3e0f868e050aada2002dead4
kashyap malvi <kashyapmalvi123@gmail.com>
2026-03-31 18:34:43 +0530

    lambda funtion

--- loops.py
+++ loops.py
@@ -1,1 +1,14 @@
-print("hola")
+addfive = lambda x:x+5
+mulfive = lambda x:x*5
+
+addtwonumber = lambda x,y : x+y
+
+name = lambda x : x+ "welcome"
+print(name("kashyap m"))
+
+
+"""
+git init
+git add .
+git commit -m "first commit"
+git branch -m Main
